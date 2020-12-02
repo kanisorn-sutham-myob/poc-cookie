f3b4e1c84f3f178c6a96ec6c54f9d8cc7a8d656b
kanisorn.sutham <kanisorn.sutham@c02c95eqmd6m.zxhn>
2020-12-02 11:02:48 +0700

    remove duplicate lock file

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
  */
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: ["http://localhost:8080", "https://poc-logout-spa.vercel.app/"],
     credentials: true,
   })
 );
